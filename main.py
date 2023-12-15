536cd9f6f4290c8b1199403afb376803383996cb
sa-mon03 <k22049kk@aitech.ac.jp>
2023-12-15 21:04:03 +0900

    日記の追加レオ

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from diaries.DiarySample import DiarySample
 from diaries.ArenDiary import ArenDiary
+from diaries.ReoDiary import ReoDiary
 # ↓のリストには、メンバーの各日記が格納されます。 
 diaries = [
     DiarySample(), 
     ArenDiary(),
+    ReoDiary(),
 ]
 
 for d in diaries:
