67ad8fcb02d13b90c46acba6ec877ec82b97697f
Tomoki-Mizuno <buddyfightlovenge27@icloud.com>
2023-12-16 23:09:37 +0900

    水野日記作成

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from diaries.DiarySample import DiarySample
 from diaries.ArenDiary import ArenDiary
 from diaries.ReoDiary import ReoDiary
+from diaries.MizunoDiary import MizunoDiary
 # ↓のリストには、メンバーの各日記が格納されます。 
 diaries = [
     DiarySample(), 
     ArenDiary(),
     ReoDiary(),
+    MizunoDiary(),
 ]
 
 for d in diaries:
